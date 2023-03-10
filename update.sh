5eddda36f00bd72d91f4445e8cc02a667d48b189
DasULTRAS <66926127+DasULTRAS@users.noreply.github.com>
2023-03-10 22:48:49 +0100

    Added dist-upgrade to bullseye

--- update.sh
+++ update.sh
@@ -12,13 +12,13 @@ function ctrl_c() {
 }
 
 if [ `cat /etc/apt/sources.list | egrep "^deb|^#" | wc -l` -le 4 ]; then
-    echo "# debian" >> /etc/apt/sources.list
+    echo "# stretch" >> /etc/apt/sources.list
 fi
 
 DEBIAN_FRONTEND=noninteractive
 state="`tail -1 /etc/apt/sources.list | cut -d' ' -f2 | egrep -v 'http'`"
 
-debian () {
+stretch () {
 tar -zcvf ~/sources.tgz /etc/apt/sources.list.d/
 rm -rfv /etc/apt/sources.list.d/*
 dpkg-reconfigure dash #Select NO Here
@@ -36,102 +36,48 @@ apt -y purge ubnt-archive-keyring ubnt-unifi-setup unifi
 apt -y purge rfkill bt-proxy bluez openjdk-8-jre-headless:armhf
 apt -y purge freeradius freeradius-common freeradius-ldap freeradius-utils bind9-host
 apt -y purge libldap-common liblocale-gettext-perl
+apt -y purge aufs-tools
 apt -y purge busybox*
 apt-get -y autoremove
-# echo "# xenial" >> /etc/apt/sources.list
+apt update
+apt dist-upgrade
+echo "# buster" >> /etc/apt/sources.list
 reboot
 }
 
-xenial () {
+buster () {
 lsb_release -a
 cat << EOF > /etc/apt/sources.list
-deb http://ports.ubuntu.com/ubuntu-ports xenial main restricted universe multiverse
-deb http://ports.ubuntu.com/ubuntu-ports xenial-updates main restricted universe multiverse
-deb http://ports.ubuntu.com/ubuntu-ports xenial-backports main restricted universe multiverse
-deb http://ports.ubuntu.com/ubuntu-ports xenial-security main restricted universe multiverse
+deb http://deb.debian.org/debian/ buster main contrib non-free
+deb http://deb.debian.org/debian/ buster-updates main contrib non-free
+deb http://deb.debian.org/debian/ buster-backports main
+deb http://security.debian.org/ buster/updates main contrib non-free
 EOF
 apt update
 apt -y upgrade
+apt dist-upgrade
 apt -y full-upgrade
 apt -y autoremove
-echo "# bionic" >> /etc/apt/sources.list
+echo "# bullseye" >> /etc/apt/sources.list
 reboot
 }
 
-bionic () {
+bullseye () {
 lsb_release -a
 cat << EOF > /etc/apt/sources.list
-deb http://ports.ubuntu.com/ubuntu-ports bionic main restricted universe multiverse
-deb http://ports.ubuntu.com/ubuntu-ports bionic-updates main restricted universe multiverse
-deb http://ports.ubuntu.com/ubuntu-ports bionic-backports main restricted universe multiverse
-deb http://ports.ubuntu.com/ubuntu-ports bionic-security main restricted universe multiverse
+deb http://deb.debian.org/debian/ bullseye main contrib non-free
+deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free
+deb http://deb.debian.org/debian/ bullseye-backports main
+deb http://security.debian.org/ bullseye/updates main contrib non-free
 EOF
 apt update
 apt -y upgrade
+apt dist-upgrade
 apt -y full-upgrade
 apt -y autoremove
-echo "# focal" >> /etc/apt/sources.list
+echo "# bullseye" >> /etc/apt/sources.list
 reboot
 }
-
-focal () {
-lsb_release -a
-cat << EOF > /etc/apt/sources.list
-deb http://ports.ubuntu.com/ubuntu-ports focal main restricted universe multiverse
-deb http://ports.ubuntu.com/ubuntu-ports focal-updates main restricted universe multiverse
-deb http://ports.ubuntu.com/ubuntu-ports focal-backports main restricted universe multiverse
-deb http://ports.ubuntu.com/ubuntu-ports focal-security main restricted universe multiverse
-EOF
-apt update
-apt -y install libcrypt1 libcryptsetup12 libhcrypto4-heimdal libgcrypt20 libk5crypto3
-apt -y upgrade
-apt -y full-upgrade
-apt -y autoremove
-echo "# clean" >> /etc/apt/sources.list
-reboot
-sleep 10
-echo "Press and hold the power button until the system powers off..."
-}
-
-clean () {
-lsb_release -a
-apt update
-apt -y upgrade
-apt -y full-upgrade
-apt -y autoremove
-cat << EOF > /etc/apt/sources.list
-deb http://ports.ubuntu.com/ubuntu-ports focal main restricted universe multiverse
-deb http://ports.ubuntu.com/ubuntu-ports focal-updates main restricted universe multiverse
-deb http://ports.ubuntu.com/ubuntu-ports focal-backports main restricted universe multiverse
-deb http://ports.ubuntu.com/ubuntu-ports focal-security main restricted universe multiverse
-EOF
-#echo "# jammy" >> /etc/apt/sources.list
-reboot
-}
-
-#jammy () {
-# This area will halfway break the system.  It cant use usrmerge, cant redo the /bin dir.  apt update shows the system
-# is up2date, but any attempts to install additional software, you get complaints from apt.
-
-# Added to upgrade to 22.04
-# apt install update-manager-core
-# do-release-upgrade -d
-
-# I will try it again without using the update manager
-#lsb_release -a
-#cat << EOF > /etc/apt/sources.list
-#deb http://ports.ubuntu.com/ubuntu-ports jammy main restricted universe multiverse
-#deb http://ports.ubuntu.com/ubuntu-ports jammy-updates main restricted universe multiverse
-#deb http://ports.ubuntu.com/ubuntu-ports jammy-backports main restricted universe multiverse
-#deb http://ports.ubuntu.com/ubuntu-ports jammy-security main restricted universe multiverse
-#EOF
-#apt update
-#apt list --upgradable | egrep jammy | cut -d"/" -f1 | egrep -v "^lib"> upgrade.list; for file in `cat upgrade.list`; do echo -en "\n Installing $file \n" $file;apt -y install $file;done
-#apt -y upgrade
-#apt -y full-upgrade
-#apt -y autoremove
-#reboot
-#}
 
 if [ -z $state ]; then
         echo "Latest tested version installed..."
