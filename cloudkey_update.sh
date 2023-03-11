5c5eed0f337d51f7bbcec07f26ca9fdbaf01a8dc
DasULTRAS <66926127+DasULTRAS@users.noreply.github.com>
2023-03-11 08:14:31 +0100

    removed service

--- cloudkey_update.sh
+++ cloudkey_update.sh
@@ -12,40 +12,24 @@ function ctrl_c() {
 }
 
 if [ `cat /etc/apt/sources.list | egrep "^deb|^#" | wc -l` -le 4 ]; then
-    echo "# init" >> /etc/apt/sources.list
+    echo "# stretch" >> /etc/apt/sources.list
 fi
 
+lsb_release -a
 DEBIAN_FRONTEND=noninteractive
 state="`tail -1 /etc/apt/sources.list | cut -d' ' -f2 | egrep -v 'http'`"
 
-init () {
+stretch () {
 tar -zcvf ~/sources.tgz /etc/apt/sources.list.d/
 rm -rfv /etc/apt/sources.list.d/*
 dpkg-reconfigure dash #Select NO Here
-lsb_release -a
-cat << EOF > /etc/systemd/system/cloudkey_update.service
-[Unit]
-Description=CloudKey Version Update
-
-[Service]
-ExecStart=~/update.sh
-
-[Install]
-WantedBy=multi-user.target
-EOF
-echo "# stretch" >> /etc/apt/sources.list
-systemctl enable cloudkey_update.service
-systemctl start cloudkey_update.service
-}
-
-stretch () {
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32 871920D1991BC93C 648ACFD622F3D138
 cat << EOF > /etc/apt/sources.list
 deb http://deb.debian.org/debian/ stretch main contrib non-free
 deb http://deb.debian.org/debian/ stretch-updates main contrib non-free
 deb http://deb.debian.org/debian/ stretch-backports main
 deb http://security.debian.org/ stretch/updates main contrib non-free
 EOF
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32 871920D1991BC93C 648ACFD622F3D138
 apt update
 apt -y purge mongodb-clients mongodb-server
 apt -y purge ubnt-archive-keyring ubnt-unifi-setup unifi
@@ -63,7 +47,6 @@ reboot
 }
 
 buster () {
-lsb_release -a
 cat << EOF > /etc/apt/sources.list
 deb http://deb.debian.org/debian/ buster main contrib non-free
 deb http://deb.debian.org/debian/ buster-updates main contrib non-free
@@ -79,7 +62,6 @@ reboot
 }
 
 bullseye () {
-lsb_release -a
 cat << EOF > /etc/apt/sources.list
 deb http://deb.debian.org/debian stable main contrib non-free
 deb-src http://deb.debian.org/debian stable main contrib non-free
@@ -90,15 +72,6 @@ apt update
 apt -y upgrade
 apt -y dist-upgrade
 apt -y autoremove
-echo "# finish" >> /etc/apt/sources.list
-reboot
-}
-
-finish (){
-systemctl stop cloudkey_update.service
-systemctl disable cloudkey_update.service
-systemctl daemon-reload
-rm /etc/systemd/system/cloudkey_update.service
 reboot
 }
 
