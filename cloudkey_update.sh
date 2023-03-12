b4fb456a4750701953cbdd314430c64647fa42e4
DasULTRAS <66926127+DasULTRAS@users.noreply.github.com>
2023-03-12 01:02:47 +0100

    Remove bullseye update

--- cloudkey_update.sh
+++ cloudkey_update.sh
@@ -48,25 +48,10 @@ reboot
 
 buster () {
 cat << EOF > /etc/apt/sources.list
-deb http://deb.debian.org/debian/ buster main contrib non-free
-deb http://deb.debian.org/debian/ buster-updates main contrib non-free
-deb http://deb.debian.org/debian/ buster-backports main
-deb http://security.debian.org/ buster/updates main contrib non-free
-EOF
-apt update
-apt -y upgrade
-apt -y dist-upgrade
-apt -y autoremove
-echo "# bullseye" >> /etc/apt/sources.list
-reboot
-}
-
-bullseye () {
-cat << EOF > /etc/apt/sources.list
-deb http://deb.debian.org/debian stable main contrib non-free
-deb-src http://deb.debian.org/debian stable main contrib non-free
-deb http://security.debian.org/debian-security stable-security main contrib non-free
-deb-src http://security.debian.org/debian-security stable-security main contrib non-free
+deb http://deb.debian.org/debian buster main contrib non-free
+deb-src http://deb.debian.org/debian buster main contrib non-free
+deb http://security.debian.org/debian-security buster/updates main contrib non-free
+deb-src http://security.debian.org/debian-security buster/updates main contrib non-free
 EOF
 apt update
 apt -y upgrade
